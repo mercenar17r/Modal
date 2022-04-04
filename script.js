005506eee59cee5c5dc9502365129c5550932535
Cosmin Negrut <cosminnegrut2002@gmai.com>
2022-04-04 22:23:05 +0300

    .

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModel = document.querySelector(`.close-modal`);
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+const closeModal = function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener(`click`, function () {
+    console.log(`Button Clicked`);
+    modal.classList.remove(`hidden`);
+    overlay.classList.remove(`hidden`);
+  });
+
+btnCloseModel.addEventListener(`click`, closeModal);
+
+overlay.addEventListener(`click`, closeModal);
