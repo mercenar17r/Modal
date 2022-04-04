892e7249d043d4254a1d8b7ccc24dfe95c4a42c6
Cosmin Negrut <cosminnegrut2002@gmai.com>
2022-04-04 22:30:44 +0300

    refactoring the code

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ const modal = document.querySelector(`.modal`);
 const overlay = document.querySelector(`.overlay`);
 const btnCloseModel = document.querySelector(`.close-modal`);
 const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+
+const openModal = function () {
+  console.log(`Button Clicked`);
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+
 const closeModal = function () {
   modal.classList.add(`hidden`);
   overlay.classList.add(`hidden`);
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener(`click`, function () {
-    console.log(`Button Clicked`);
-    modal.classList.remove(`hidden`);
-    overlay.classList.remove(`hidden`);
-  });
+  btnsOpenModal[i].addEventListener(`click`, openModal);
 
 btnCloseModel.addEventListener(`click`, closeModal);
 
